0aa8d59a98b200a887b736dc0c7efb28eb2f292b
frypf <65832791+frypf@users.noreply.github.com>
2023-02-23 09:24:02 +0000

    FIX: activityBar tag counts.
    • Items with `shouldHideFromActivityBar === false` were previously only shown if they also had `shouldHideFromStatusBar === false`.

--- src/extension.js
+++ src/extension.js
@@ -167,19 +167,6 @@ function activate( context )
         var badgeTotal = config.shouldShowActivityBarBadge() ? total : 0;
         todoTreeView.badge = { value: badgeTotal };
 
-        if( statusBar === STATUS_BAR_CURRENT_FILE )
-        {
-            var fileFilter;
-
-            if( vscode.window.activeTextEditor && vscode.window.activeTextEditor.document )
-            {
-                fileFilter = vscode.window.activeTextEditor.document.fileName;
-            }
-
-            counts = provider.getTagCountsForStatusBar( fileFilter );
-            total = Object.values( counts ).reduce( function( a, b ) { return a + b; }, 0 );
-        }
-
         var countRegex = new RegExp( "([^(]*)(\\(\\d+\\))*" );
         var match = countRegex.exec( todoTreeView.title );
         if( match !== null )
@@ -204,6 +191,19 @@ function activate( context )
             }
             todoTreeView.title = title;
         }
+
+        if( statusBar === STATUS_BAR_CURRENT_FILE )
+        {
+            var fileFilter;
+
+            if( vscode.window.activeTextEditor && vscode.window.activeTextEditor.document )
+            {
+                fileFilter = vscode.window.activeTextEditor.document.fileName;
+            }
+
+            counts = provider.getTagCountsForStatusBar( fileFilter );
+            total = Object.values( counts ).reduce( function( a, b ) { return a + b; }, 0 );
+        }    
 
         if( statusBar === STATUS_BAR_TOTAL )
         {
